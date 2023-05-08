c0b394ea4a72ae721e3cd94ae05561d255623051
oğuzhan <pekgozoguzhan94@gmail.com>
2023-05-08 18:38:59 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotlar
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kalan Hesaplama İçin Bölünen Sayıyı Giriniz: ");
+            int bolunen = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Kalan Hesaplmaa İçin Bölen Sayıyı Giriniz:");
+            int bolen = Convert.ToInt32(Console.ReadLine());
+            int kalan = bolunen % bolen;
+
+            Console.WriteLine("{0} sayısının {1}e'e bölümünden kalan : {2} dir", bolunen, bolen, kalan);
+
+            Console.ReadKey();
+        }
+    }
+}
+
